8e9d56f86186ceec8f71b768f04c9c8e72090bc0
schalkkim <schalk.kim@awatapu.school.nz>
2022-03-03 11:08:03 +1300

    Scrollbar added

--- help_window_framework.py
+++ help_window_framework.py
@@ -18,7 +18,24 @@ root.configure(bg=background_colour)
 # Define Frames
 # Groups the help information
 information_frame = Frame(root, bg=box_colour, bd=3)
-information_frame.grid(row=1, column=0, padx=15, pady=(10, 15), stick="nw")
+information_frame.grid(row=1, column=0, padx=20, pady=(10, 15), stick="nw")
+information_frame.grid_rowconfigure(0, weight=1)
+information_frame.grid_columnconfigure(0, weight=1)
+information_frame.grid_propagate(False)
+
+# Add canvas, so scrollbar can be added
+information_canvas = Canvas(information_frame, bg=box_colour)
+information_canvas.grid(row=0, column=0, sticky="news")
+
+# Create Scrollbar
+information_scrollbar = Scrollbar(information_frame, orient="vertical", command=information_canvas.yview)
+information_scrollbar.grid(row=0, column=1, stick="ns", padx=5, pady=5)
+
+information_canvas.configure(yscrollcommand=information_scrollbar.set)
+
+# Add a frame for the information labels
+information_labels_frame = Frame(information_canvas, bg=box_colour)
+information_canvas.create_window((0, 0), window=information_labels_frame, anchor="nw")
 
 # Define Labels
 help_label = Label(root, text="Help", fg="black", bg=background_colour, font=("Arial", 20, "bold"), justify=CENTER)
@@ -31,13 +48,13 @@ If an error appears, check that your number is in the format ‘7’ rather than
 
 To exit, press the ‘x’ at the top of the window, this will close the program.
 """)
-information_label = Label(information_frame, textvariable=information_textvariable, bg=box_colour, bd=0, fg="black",
-                          font=("Arial", 11), wraplength=300, justify=LEFT)
+information_label = Label(information_labels_frame, textvariable=information_textvariable, bg=box_colour, bd=0, fg="black",
+                          font=("Arial", 11), wraplength=260, justify=LEFT)
 information_label.grid(row=0, column=0)
 
-history_label = Label(information_frame, text="History", bg=box_colour, bd=0, fg="black", font=("Arial", 11, "bold"),
+history_label = Label(information_labels_frame, text="History", bg=box_colour, bd=0, fg="black", font=("Arial", 11, "bold"),
                       justify=LEFT)
-history_label.grid(row=1, column=0, padx=(0, 245))
+history_label.grid(row=1, column=0, padx=(0, 205))
 
 history_information_textvariable = StringVar()
 history_information_textvariable.set("""Press ‘View history’ to see your last 8 conversions. To view more you can export your history by pressing the ‘Export’ button. Once in that window type in your filename and press export. Your file will appear in the folder where this program is located. 
@@ -45,14 +62,23 @@ history_information_textvariable.set("""Press ‘View history’ to see your las
 To clear your history, press the ‘Clear’ button. This will delete your history from the program, however, if you have a file saved, the history will not be deleted from there. You will have to delete the file by going to the folder it is located in and deleting it there.
 
 When finished press the back button to return to the home window.""")
-history_information_label = Label(information_frame, textvariable=information_textvariable, bg=box_colour, bd=0,
-                                  fg="black", font=("Arial", 11), wraplength=300, justify=LEFT)
+history_information_label = Label(information_labels_frame, textvariable=history_information_textvariable, bg=box_colour, bd=0,
+                                  fg="black", font=("Arial", 11), wraplength=260, justify=LEFT)
 history_information_label.grid(row=2, column=0)
 
 # Define Button
 back_button = Button(root, text="Back", fg="black", bg=background_colour, bd=0, font=("Arial", 11, "underline"),
                      justify=LEFT, activebackground=background_colour)
-back_button.grid(row=2, column=0, padx=(10, 275), pady=(5, 10))
+back_button.grid(row=2, column=0, padx=(10, 285), pady=(5, 10))
+
+# Allows resizing of labels
+information_labels_frame.update_idletasks()
+
+# Change size of the box
+information_frame.config(width=310, height=350)
+
+# Set scrolling region
+information_canvas.config(scrollregion=information_canvas.bbox("all"))
 
 # Close window
 root.mainloop()
