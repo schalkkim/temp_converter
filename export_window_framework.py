524447507dadf4e2970435761ce13996fa8a1946
schalkkim <schalk.kim@awatapu.school.nz>
2022-03-08 09:42:13 +1300

    Entry Added

--- export_window_framework.py
+++ export_window_framework.py
@@ -15,7 +15,7 @@ entry_colour = "#FCE5CD"
 root = Tk()
 root.title("Temperature Converter")
 # Design window
-root.configure(bg=background_colour)
+root.configure(bg=background_colour, padx=10, pady=5)
 
 # Define Labels
 export_history_label = Label(root, text="Export History", fg="black", bg=background_colour, font=("Arial", 20, "bold"),
@@ -25,8 +25,15 @@ export_history_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 export_information_variable = StringVar()
 export_information_variable.set("Type in the desired file name and press export to convert the history to a txt file.")
 export_information_label = Label(root, textvariable=export_information_variable, fg="black", bg=background_colour,
-                                 font=("Arial", 11, "italic"), justify=LEFT, wraplength=350)
+                                 font=("Arial", 11, "italic"), justify=LEFT, wraplength=310)
 export_information_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+
+# Define Entries and Variables
+file_name_variable = DoubleVar()
+file_name_variable.set("")
+file_name_entry = Entry(root, textvariable=file_name_variable, fg="black", bg=entry_colour, width=38,
+                        font=("Arial", 11), justify=LEFT, bd=3)
+file_name_entry.grid(row=2, column=0, columnspan=2, pady=5, padx=5, ipadx=2, ipady=2)
 
 # Close Window
 root.mainloop()
